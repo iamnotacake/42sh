c93c25c0afecb8d567a9758394ff7754f383d836
Oleksiy Lyubochko <olyuboch@e3r5p8.unit.ua>
2017-09-26 17:37:47 +0300

    fix redir

--- ft_preprocessing/ft_pre_redirection.c
+++ ft_preprocessing/ft_pre_redirection.c
@@ -12,7 +12,7 @@
 
 #include "ft_preprocessing_private.h"
 
-void	ft_pre_redir_fd(t_syntax_tree *tree, t_proc **proc, int num2)
+void	ft_pre_redir_fd(t_syntax_tree *tree, t_proc **proc, int num2, int def)
 {
 	int	num1;
 
@@ -20,7 +20,7 @@ void	ft_pre_redir_fd(t_syntax_tree *tree, t_proc **proc, int num2)
 	if (!ft_strcmp(tree->args[1], "-"))
 	{
 		if ((num1 = open("/dev/null", O_WRONLY |
-			O_CREAT | O_TRUNC, 0640)) < 2)
+			O_CREAT | def, 0640)) < 2)
 		{
 			ft_pre_permission_error(tree, proc);
 			return ;
@@ -35,18 +35,23 @@ void	ft_pre_redir_fd(t_syntax_tree *tree, t_proc **proc, int num2)
 	(*proc)->dup[num2] = num1;
 }
 
-void	ft_pre_write_fd(t_syntax_tree *tree, t_proc **proc)
+void	ft_pre_write_fd(t_syntax_tree *tree, t_proc **proc, char w)
 {
 	int	fd;
 	int	num2;
+	int	def;
 
+	if (w == 'w')
+		def = O_TRUNC;
+	if (w == 'a')
+		def = O_APPEND;
 	num2 = ft_atoi(tree->args[0] + 1);
 	if (!ft_strcmp(tree->args[1], "-") || tree->args[1][0] == '&')
-		ft_pre_redir_fd(tree, proc, num2);
+		ft_pre_redir_fd(tree, proc, num2, def);
 	else
 	{
 		if ((fd = open(tree->args[1], O_WRONLY |
-			O_CREAT | O_TRUNC, 0640)) < 2)
+			O_CREAT | def, 0640)) < 2)
 			ft_pre_permission_error(tree, proc);
 		else
 		{
@@ -57,23 +62,27 @@ void	ft_pre_write_fd(t_syntax_tree *tree, t_proc **proc)
 	}
 }
 
-void	ft_pre_append_fd(t_syntax_tree *tree, t_proc **proc)
-{
-	int	fd;
+// void	ft_pre_append_fd(t_syntax_tree *tree, t_proc **proc)
+// {
+// 	int	fd;
 
-	if (tree->args[0][1] == '1')
-	{
-		if ((fd = open(tree->args[1], O_WRONLY |
-			O_CREAT | O_APPEND, 0640)) < 2)
-			ft_pre_permission_error(tree, proc);
-		else
-		{
-			if ((*proc)->dup[1] != 1 && (*proc)->dup[1] > 2)
-				close((*proc)->dup[1]);
-			(*proc)->dup[1] = fd;
-		}
-	}
-}
+// 	int	num2;
+// 	num2 = ft_atoi(tree->args[0] + 1);
+// 	if (!ft_strcmp(tree->args[1], "-") || tree->args[1][0] == '&')
+// 		ft_pre_redir_fd(tree, proc, num2, 2);
+// 	else
+// 	{
+// 		if ((fd = open(tree->args[1], O_WRONLY |
+// 			O_CREAT | O_APPEND, 0640)) < 2)
+// 			ft_pre_permission_error(tree, proc);
+// 		else
+// 		{
+// 			if ((*proc)->dup[num2] != num2 && (*proc)->dup[num2] > 2)
+// 				close((*proc)->dup[1]);
+// 			(*proc)->dup[num2] = fd;
+// 		}
+// 	}
+// }
 
 void	ft_pre_read_fd(t_syntax_tree *tree, t_proc **proc)
 {
@@ -94,12 +103,12 @@ void	ft_pre_read_fd(t_syntax_tree *tree, t_proc **proc)
 
 void	ft_pre_redirection(t_syntax_tree *tree, t_proc **proc)
 {
-	if (tree->args[0][0] == 'w')
-		ft_pre_write_fd(tree, proc);
-	else if (tree->args[0][0] == 'a')
-		ft_pre_append_fd(tree, proc);
+	if (tree->args[0][0] == 'w' || tree->args[0][0] == 'a')
+		ft_pre_write_fd(tree, proc, tree->args[0][0]);
 	else if (tree->args[0][0] == 'r')
 		ft_pre_read_fd(tree, proc);
 	else if (tree->args[0][0] == 'h')
 		ft_pre_heredoc_fd(tree, proc);
+	// else if (tree->args[0][0] == 'a')
+	// 	ft_pre_append_fd(tree, proc);
 }
