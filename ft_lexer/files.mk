d7ecf6e2a2c2147f665a528dd1ab0f25dd110207
Mikhailo Varga <mvarga@e3r5p10.unit.ua>
2017-09-28 18:12:04 +0300

    ft_lexer norm

--- ft_lexer/files.mk
+++ ft_lexer/files.mk
@@ -1,7 +1,18 @@
+#******************************************************************************#
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    files.mk                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mvarga <marvin@42.fr>                      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/09/28 18:09:21 by mvarga            #+#    #+#              #
+#    Updated: 2017/09/28 18:09:25 by mvarga           ###   ########.fr        #
+#                                                                              #
+#******************************************************************************#
+
 CFLAGS += -Ift_lexer
 LDFLAGS += -ll
 SOURCES += ft_lexer/ft_lexer_lex.c \
 		   ft_lexer/ft_lexer.c \
 		   ft_lexer/ft_lexer_post_string.c \
 		   ft_lexer/ft_lexer_resolve_vars.c \
-
