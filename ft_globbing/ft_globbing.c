cc270082c10b48ddf6ae4e4fe31e0fa278cb348a
Mikhailo Varga <mvarga@e3r5p10.unit.ua>
2017-09-28 17:10:36 +0300

    globing norm

--- ft_globbing/ft_globbing.c
+++ ft_globbing/ft_globbing.c
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_globbing.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvarga <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/28 16:45:51 by mvarga            #+#    #+#             */
+/*   Updated: 2017/09/28 17:02:53 by mvarga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_globbing.h"
 
 static int			ft_do_globing(char *tokens, int flag, int cur_char, int c)
 {
 	char			*pwd;
 	struct dirent	*dirbuf;
-	DIR 			*dfd;
+	DIR				*dfd;
 
 	pwd = ft_env_get(g_env_g, "PWD");
 	if ((dfd = opendir(pwd)) == NULL)
 		return (0);
-	while ((dirbuf = readdir(dfd)) != NULL )
+	while ((dirbuf = readdir(dfd)) != NULL)
 	{
 		if (dirbuf->d_ino == 0)
 			continue;
-		if (ft_strcmp(dirbuf->d_name, ".") == 0 || ft_strcmp(dirbuf->d_name, "..") == 0 
-			|| *(dirbuf->d_name) == '.')
+		if (ft_strcmp(dirbuf->d_name, ".") == 0 ||\
+			ft_strcmp(dirbuf->d_name, "..") == 0 || *(dirbuf->d_name) == '.')
 			continue;
 		if (ft_match(dirbuf->d_name, tokens, cur_char, c))
 		{
@@ -26,13 +38,12 @@ static int			ft_do_globing(char *tokens, int flag, int cur_char, int c)
 	return (flag);
 }
 
-int				ft_globbing_is_pattern(char *pattern)
+int					ft_globbing_is_pattern(char *pattern)
 {
 	static char		globchars[] = "*?[{";
 	char			*p;
-	int 			cur_char;
+	int				cur_char;
 	int				c;
-
 
 	cur_char = 0;
 	c = 0;
--- ft_globbing/ft_globbing.h
+++ ft_globbing/ft_globbing.h
@@ -1,19 +1,31 @@
-#ifndef __FT_GLOBBING
-# define __FT_GLOBBING
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_globbing.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvarga <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/28 16:46:34 by mvarga            #+#    #+#             */
+/*   Updated: 2017/09/28 16:57:48 by mvarga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GLOBBING_H
+# define FT_GLOBBING_H
 
 # include <stdlib.h>
 # include <string.h>
 # include "ft_parser.h"
 # include <dirent.h>
 # include "ft_env.h"
-#include "libft.h"
+# include "libft.h"
 
-#define U(c) ((c) & 0377)
+# define U(c) ((c) & 0377)
 
 void			ft_globbing_call(t_syntax_tree *arguments);
 void			ft_globbing_callback(char *str);
 int				ft_globbing_is_pattern(char *pattern);
-int 			ft_match(char *s, char *p, int cur_char, int c);
+int				ft_match(char *s, char *p, int cur_char, int c);
 int				ft_brackets(char **p, int cur_char);
 
 #endif
