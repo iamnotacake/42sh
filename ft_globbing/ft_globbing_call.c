8434bd684cd32678fbf6bb28b5efb399ad111ac8
Nikita Braslavskiy <nikita.braslavskiy@gmail.com>
2017-10-03 20:38:08 +0300

    - memmove, + ft_memmove

--- ft_globbing/ft_globbing_call.c
+++ ft_globbing/ft_globbing_call.c
@@ -13,8 +13,8 @@
 #include "ft_globbing.h"
 #include "ft_free.h"
 
-int				g_index;
-t_syntax_tree	*g_tree;
+int				g_index2;
+t_syntax_tree	*g_tree2;
 
 static int		ptr_arr_len(t_syntax_tree **ptrs)
 {
@@ -31,7 +31,7 @@ static void		ptr_arr_remove(t_syntax_tree **tree, int index)
 	int			len;
 
 	len = ptr_arr_len(tree);
-	memmove(&tree[index],
+	ft_memmove(&tree[index],
 			&tree[index + 1],
 			sizeof(void *) * (len - index + 1));
 }
@@ -42,7 +42,7 @@ static void		**ptr_arr_insert(void **ptrs, int index, void *ptr)
 
 	len = ptr_arr_len((t_syntax_tree **)ptrs);
 	ptrs = realloc(ptrs, sizeof(void *) * (len + 2));
-	memmove((void *)&ptrs[index + 1],
+	ft_memmove((void *)&ptrs[index + 1],
 			(void *)&ptrs[index],
 			sizeof(void *) * (len - index + 1));
 	ptrs[index] = ptr;
@@ -55,9 +55,9 @@ void			ft_globbing_callback(char *str)
 
 	elem = syntax_tree_new("string");
 	syntax_tree_append(elem, NULL, str);
-	g_tree->tree = (t_syntax_tree **)ptr_arr_insert((void **)g_tree->tree,
-													g_index, elem);
-	g_index += 1;
+	g_tree2->tree = (t_syntax_tree **)ptr_arr_insert((void **)g_tree2->tree,
+													g_index2, elem);
+	g_index2 += 1;
 }
 
 void			ft_globbing_call(t_syntax_tree *arguments)
@@ -65,20 +65,20 @@ void			ft_globbing_call(t_syntax_tree *arguments)
 	char		*tmp;
 
 	ft_tilda_exchange(arguments);
-	g_index = 0;
-	g_tree = arguments;
-	while (arguments->tree[g_index])
+	g_index2 = 0;
+	g_tree2 = arguments;
+	while (arguments->tree[g_index2])
 	{
-		if (arguments->tree[g_index]->need_globbing &&
-			arguments->tree[g_index]->args[0] &&
-			ft_globbing_is_pattern(arguments->tree[g_index]->args[0]))
+		if (arguments->tree[g_index2]->need_globbing &&
+			arguments->tree[g_index2]->args[0] &&
+			ft_globbing_is_pattern(arguments->tree[g_index2]->args[0]))
 		{
-			tmp = ft_strdup(arguments->tree[g_index]->args[0]);
-			ft_free_syntax_tree(arguments->tree[g_index]);
-			ptr_arr_remove(arguments->tree, g_index);
+			tmp = ft_strdup(arguments->tree[g_index2]->args[0]);
+			ft_free_syntax_tree(arguments->tree[g_index2]);
+			ptr_arr_remove(arguments->tree, g_index2);
 			free(tmp);
 		}
 		else
-			g_index += 1;
+			g_index2 += 1;
 	}
 }
