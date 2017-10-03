0a2666ae8ca6f65e78a526fb92c5106d917e121e
Nikita Braslavskiy <nikita.braslavskiy@gmail.com>
2017-10-03 16:55:30 +0300

    norme ft_parser/ft_parser_core.c

--- ft_parser/ft_parser_core.c
+++ ft_parser/ft_parser_core.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parser_core.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbraslav <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/03 16:51:15 by mbraslav          #+#    #+#             */
+/*   Updated: 2017/10/03 16:51:18 by mbraslav         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_parser_private.h"
 
 extern t_token	*g_prev_tok;
@@ -37,7 +49,8 @@ int				parser_accept(t_token_type type,
 		if (tree)
 		{
 			syntax_tree_append(tree, NULL,
-					strdup(g_curr_sym->data ?: g_curr_sym->match));
+					strdup(g_curr_sym->data ? g_curr_sym->data : \
+														g_curr_sym->match));
 			tree->need_globbing = g_curr_sym->type == T_STRING &&
 				g_curr_sym->subtype == ST_NONE;
 		}
@@ -47,18 +60,20 @@ int				parser_accept(t_token_type type,
 	return (0);
 }
 
-void			ft_tree_print(t_syntax_tree *tree, int level)
-{
-	for (int i = 0; i < level; i++) printf(". ");
-	printf("<"C137"%s"C0">\n", tree ? tree->type : "NULL");
-	if (!tree)
-		return ;
-	for (int i = 0; tree->tree[i]; i++)
-		ft_tree_print(tree->tree[i], level + 1);
-	for (int i = 0; tree->args[i]; i++)
-	{
-		for (int i = 0; i < level; i++) printf(". ");
-		printf("%s"C036"args"C0"["C032"%d"C0"]: "C033"%s"C0"\n",
-				tree->args[i + 1] ? "┣ " : "┗ ", i, tree->args[i]);
-	}
-}
+/*
+** void			ft_tree_print(t_syntax_tree *tree, int level)
+** {
+** 	for (int i = 0; i < level; i++) printf(". ");
+** 	printf("<"C137"%s"C0">\n", tree ? tree->type : "NULL");
+** 	if (!tree)
+** 		return ;
+** 	for (int i = 0; tree->tree[i]; i++)
+** 		ft_tree_print(tree->tree[i], level + 1);
+** 	for (int i = 0; tree->args[i]; i++)
+** 	{
+** 		for (int i = 0; i < level; i++) printf(". ");
+** 		printf("%s"C036"args"C0"["C032"%d"C0"]: "C033"%s"C0"\n",
+** 				tree->args[i + 1] ? "┣ " : "┗ ", i, tree->args[i]);
+** 	}
+** }
+*/
